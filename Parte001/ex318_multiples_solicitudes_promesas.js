ab8ecfdeb6f1acffc52e6d992ea191c14216b5ce
John Ortiz Ordoñez <johnortizo@outlook.com>
2020-01-20 16:25:12 -0500

    JavaScript - Ejercicio 318
    JavaScript - Ejercicio 318: Ejecutar Múltiples Solicitudes GET como Objetos Promise usando all().

--- Parte001/ex318_multiples_solicitudes_promesas.js
+++ Parte001/ex318_multiples_solicitudes_promesas.js
@@ -1,3 +1,8 @@
 // Ejercicio 318: Ejecutar múltiples solicitudes GET como objetos Promise usando all().
 
+let urls = ['https://randomuser.me/api/?results=3', 'https://randomuser.me/api/?results=5', 'https://randomuser.me/api/?results=10']
 
+let solicitudes = urls.map(url => fetch(url));
+
+Promise.all(solicitudes)
+.then(respuestas => respuestas.forEach(r => console.log(`${r.url}: ${r.status}`)));
