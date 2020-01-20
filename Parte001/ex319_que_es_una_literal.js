b43bcb10c1f69b049d46d423cfb5262e667cf4a3
John Ortiz Ordoñez <johnortizo@outlook.com>
2020-01-20 17:00:32 -0500

    JavaScript - Ejercicio 319
    JavaScript - Ejercicio 319: Estudiar el Concepto de Literal de un Valor Específico de un Tipo de Dato.

---
+++ Parte001/ex319_que_es_una_literal.js
@@ -0,0 +1,15 @@
+// Ejercicio 319: Estudiar el concepto de literal de un valor específico de un tipo de dato.
+
+let nombre = 'Edward';
+let numero = 2;
+let precio = 29.13;
+let llueve = false;
+let corre = true;
+let persona = {'nombre': 'Edward', 'edad': 29};
+
+console.log(typeof nombre);
+console.log(typeof numero);
+console.log(typeof precio);
+console.log(typeof llueve);
+console.log(typeof corre);
+console.log(typeof persona);
