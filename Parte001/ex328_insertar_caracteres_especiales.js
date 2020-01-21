399e7b35f1d0588cadc141ca82986b18ca63d1c9
John Ortiz Ordoñez <johnortizo@outlook.com>
2020-01-21 11:49:25 -0500

    JavaScript - Ejercicio 328
    JavaScript - Ejercicio 328: Insertar Caracteres Especiales en una Cadena de Caracteres.

---
+++ Parte001/ex328_insertar_caracteres_especiales.js
@@ -0,0 +1,12 @@
+// Ejercicio 328: Insertar caracteres especiales en una cadena de caracteres.
+
+let texto = 'Carácter de derechos reservados: \u00A9';
+console.log(texto);
+
+console.log('Carácter de párrafo: \u00B6');
+
+texto = 'John\'s computer';
+console.log(texto);
+
+texto = "\tEl lenguaje\n\nde programación \"JavaScript\" es tremendo.";
+console.log(texto);
