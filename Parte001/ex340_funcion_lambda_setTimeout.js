114e41791674e8efba098c781abc40947e922e14
John Ortiz Ordoñez <johnortizo@outlook.com>
2020-01-23 07:33:45 -0500

    JavaScript - Ejercicio 340
    JavaScript - Ejercicio 340: Crear una Función Lambda como Callback de la Función setTimeout().

---
+++ Parte001/ex340_funcion_lambda_setTimeout.js
@@ -0,0 +1,5 @@
+// Ejercicio 340: Crear una función lambda como callback de la función setTimeout().
+
+setTimeout(() => {
+    console.log('Se ha completado la ejecución del callback.');
+}, 5000);
