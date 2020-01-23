fec654126a5d18e751888af2853f279157f2a198
John Ortiz Ordoñez <johnortizo@outlook.com>
2020-01-23 15:59:01 -0500

    JavaScript - Ejercicio 352
    JavaScript - Ejercicio 352: Convertir un Número Decimal en un Número Hexadecimal.

---
+++ Parte001/ex352_convertir_decimal_hexadecimal.js
@@ -0,0 +1,10 @@
+// Ejercicio 352: Convertir un número decimal en un número hexadecimal.
+
+// Número en base 10:
+let decimal = 512;
+
+// Número en base 16:
+let resultado = decimal.toString(16);
+
+console.log(decimal);
+console.log(resultado);
