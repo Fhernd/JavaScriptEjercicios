e495aae5347692230e2dd203344e02878b1dd231
John Ortiz Ordoñez <johnortizo@outlook.com>
2020-01-23 16:26:00 -0500

    JavaScript - Ejercicio 353
    JavaScript - Ejercicio 353: Convertir un Número en Base 10 a Base Octal, Hexadecimal y Binaria.

---
+++ Parte001/ex353_convertir_base_10_8_16_2.js
@@ -0,0 +1,18 @@
+// Ejercicio 353: Convertir un número en base 10 a base octal, hexadecimal y binaria.
+
+// Base 10:
+let decimal = 255;
+
+// Base 8:
+let octal = decimal.toString(8);
+
+// Base 16:
+let hexadecimal = decimal.toString(16);
+
+// Base 2:
+let binario = decimal.toString(2);
+
+console.log(decimal);
+console.log(octal);
+console.log(hexadecimal);
+console.log(binario);
