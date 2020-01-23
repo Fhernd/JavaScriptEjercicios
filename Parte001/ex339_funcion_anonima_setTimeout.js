4d3ddffe9bae81acda853a0e8b7260260a59ce9e
John Ortiz Ordoñez <johnortizo@outlook.com>
2020-01-23 07:16:35 -0500

    JavaScript - Ejercicio 339
    JavaScript - Ejercicio 339: Crear una Función Anónima como Callback de la Función setTimeout().

---
+++ Parte001/ex339_funcion_anonima_setTimeout.js
@@ -0,0 +1,5 @@
+// Ejercicio 339: Crear una función anónima como callback de la función setTimeout().
+
+setTimeout(function(){
+    console.log('Se ha ejecutado el callback de la función setTimeout()');
+}, 5000);
