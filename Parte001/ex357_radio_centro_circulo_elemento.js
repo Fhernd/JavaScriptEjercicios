4bfd392f693084bd2ce02bea0d8f8c3fe85bf15e
John Ortiz Ordoñez <johnortizo@outlook.com>
2020-01-24 11:51:11 -0500

    JavaScript - Ejercicio 357
    JavaScript - Ejercicio 357: Calcular el Radio y Centor de un Círculo para Incrustar en un Elemento.

---
+++ Parte001/ex357_radio_centro_circulo_elemento.js
@@ -0,0 +1,15 @@
+// Ejercicio 357: Calcular el radio y centro de un círculo para incrustar en un elemento.
+
+let ancho = 300;
+let alto = 200;
+
+// Cálculo del radio del círculo:
+let radioCirculo = Math.min(ancho, alto) / 2;
+
+// Cálculo del centro del círculo:
+let x = ancho / 2;
+let y = alto / 2;
+
+console.log(radioCirculo);
+console.log(x);
+console.log(y);
