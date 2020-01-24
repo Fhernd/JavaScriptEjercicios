0731df2b199695cf92a2c414132d4eeb7438c73a
John Ortiz Ordoñez <johnortizo@outlook.com>
2020-01-24 09:18:13 -0500

    JavaScript - Ejercicio 356
    JavaScript - Ejercicio 356: Crear una Función Personalizada para Convertir Grados a Radianes.

---
+++ Parte001/ex356_convertir_grados_radianes.js
@@ -0,0 +1,10 @@
+// Ejercicio 356: Crear una función personalizada para convertir grados a radianes.
+
+function convertirGradosRadianes(grados){
+    return grados * (Math.PI/180);
+}
+
+console.log(convertirGradosRadianes(90));
+console.log(Math.PI / 2);
+console.log(convertirGradosRadianes(180));
+console.log(convertirGradosRadianes(360));
