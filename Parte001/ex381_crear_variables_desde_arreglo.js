b95186e607badbebac941fbbecbb9210fa5226ef
infzer0 <johnortizo@outlook.com>
2020-01-26 16:03:18 -0500

    JavaScript - Ejercicio 381
    JavaScript - Ejercicio 381: Crear Múltiples Variables a partir de los Elementos de un Arreglo.

---
+++ Parte001/ex381_crear_variables_desde_arreglo.js
@@ -0,0 +1,12 @@
+// Ejercicio 381: Crear múltiples variables a partir de los elementos de un arreglo.
+
+let lenguajes = ['JavaScript', 'Python', 'Java', 'C++', 'PHP', 'C#'];
+
+let [javascript, python, java, cpp, php, csharp] = lenguajes;
+
+console.log(javascript);
+console.log(python);
+console.log(java);
+console.log(cpp);
+console.log(php);
+console.log(csharp);
