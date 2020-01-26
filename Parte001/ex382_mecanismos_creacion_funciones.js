85e5296257b340a7402c2cf6ea023fee0599c6bd
infzer0 <johnortizo@outlook.com>
2020-01-26 17:14:42 -0500

    JavaScript - Ejercicio 382
    JavaScript - Ejercicio 382: Demostrar los Mecanismos de Creación y Definición de Funciones.

---
+++ Parte001/ex382_mecanismos_creacion_funciones.js
@@ -0,0 +1,14 @@
+// Ejercicio 382: Demostrar los mecanismos de creación y definición de funciones.
+
+// 1. Mecanismo declarativo:
+function funcion1(){
+    // Cuerpo de declaración o implementación
+}
+
+// 2. Mecanismo objeto Function:
+let funcion2 = new Function('console.log("Instancia de Function");');
+
+// 3. Mecanismo expresión de función:
+let funcion3 = function(){
+    // Cuerpo de declaración o implementación
+};
