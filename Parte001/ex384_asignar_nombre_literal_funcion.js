e67347e76133582ac6610d6bb22b0e104c3909c2
infzer0 <johnortizo@outlook.com>
2020-01-27 09:04:46 -0500

    JavaScript - Ejercicio 384
    JavaScript - Ejercicio 384: Asignar un Nombre a la Declaración de una Literal de Función.

---
+++ Parte001/ex384_asignar_nombre_literal_funcion.js
@@ -0,0 +1,10 @@
+// Ejercicio 384: Asignar un nombre a la declaración de una literal de función.
+
+let sumar = function adicion(a, b) {
+    console.log(typeof adicion);
+    let suma = a + b;
+    return suma;
+}
+
+console.log(sumar(2, 3));
+// console.log(adicion(2, 3)); // ReferenceError
