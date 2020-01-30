dab9bf6ba76e23da466bdbaca01eb5c726c05c68
infzer0 <johnortizo@outlook.com>
2020-01-30 12:19:35 -0500

    JavaScript - Ejercicio 396
    JavaScript - Ejercicio 396: Convertir los Argumentos de una Función en un Objeto Arreglo.

---
+++ Parte001/ex396_convertir_arguments_arreglo.js
@@ -0,0 +1,23 @@
+// Ejercicio 396: Convertir los argumentos de una función en un objeto arreglo.
+
+// arguments
+
+function fn(){
+    console.log(typeof arguments);
+    console.log(arguments instanceof Array);
+    console.log(arguments.length);
+
+    console.log();
+
+    let arregloArguments = [].slice.call(arguments);
+    console.log(typeof arregloArguments);
+    console.log(arregloArguments instanceof Array);
+    console.log(arregloArguments.length);
+    console.log(arregloArguments.indexOf(2));
+    console.log(arregloArguments.indexOf(7));
+    console.log('----------------------------------');
+}
+
+fn();
+console.log();
+fn(2, 3, 5);
