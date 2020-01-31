2c6317458cda70ce1cc46bfe86319541de1d149e
infzer0 <johnortizo@outlook.com>
2020-01-31 07:18:06 -0500

    JavaScript - Ejercicio 403
    JavaScript - Ejercicio 403: Creación Básica de una Función Tipo IIFE.

---
+++ Parte001/ex403_creacion_funcion_iife.js
@@ -0,0 +1,7 @@
+// Ejercicio 403: Creación básica de una función tipo IIFE.
+
+// IIFE => Immediately-Invoked Function Expression
+
+(function(){
+    console.log('La función se invocó de forma inmediata');
+})();
