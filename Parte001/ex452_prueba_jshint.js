71c3dce49dd1cf730a90cfce0c281acff0da4740
infzer0 <johnortizo@outlook.com>
2020-02-12 16:12:33 -0500

    JavaScript - Ejercicio 452
    JavaScript - Ejercicio 452: Realizar un Análisis Estático de Código JS con JSHint.

---
+++ Parte001/ex452_prueba_jshint.js
@@ -0,0 +1,18 @@
+// Ejercicio 452: Realizar un análisis estático de código JS con JSHint.
+
+function sumar(a, b, c){
+    return a + b + c;
+}
+
+// curly
+let contador = 0;
+while(contador <= 10){
+    ++contador;
+}
+
+// eqeqeq
+if (contador === 10){
+    console.log('La variable contador es igual a 10.');
+} else {
+    console.log('La variable contador NO es igual a 10.');
+}
