750aaaab6e563abf506382bc12de5fc37bf3ace9
infzer0 <johnortizo@outlook.com>
2020-03-16 18:25:39 -0500

    JavaScript - Ejercicio 554 - Parte 2/2
    JavaScript - Ejercicio 554: Parte 2/2 Exportar una Clase Personalizada desde un Módulo Node.js.

--- Parte001/ex554_exportar_clase_node.js
+++ Parte001/ex554_exportar_clase_node.js
@@ -1,3 +1,7 @@
 // Ejercicio 554: Exportar una clase personalizada desde un módulo Node.js.
 
+let modulo = require('./ex554_modulo_clase_producto');
 
+let computador = new modulo.Producto(1001, 'Computador', 'MSi', 759);
+
+console.log(computador.getDatosProducto());
