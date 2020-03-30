0d39111c987c3306738681632539bd5d4a11ad85
infzer0 <johnortizo@outlook.com>
2020-03-30 07:54:41 -0500

    JavaScript - Ejercicio 588
    JavaScript - Ejercicio 588: Usar el Módulo restify de Node.js para Construir una API REST.

---
+++ Parte001/ex588_api_rest_restify_nodejs.js
@@ -0,0 +1,23 @@
+// Ejercicio 588: Usar el módulo Node.js restify para construir una API REST.
+
+let restify = require('restify');
+
+let servidor = restify.createServer({name: 'apirest'});
+servidor.use(restify.plugins.bodyParser());
+
+servidor.get('/api/datos/:nombre', function retrieve(req, res, next){
+    res.send('El dato enviado es: ' + req.params.nombre);
+    next();
+});
+
+servidor.post('/api/datos/crear', function create(req, res, next) {
+    console.log(req.params);
+
+    res.send('Se ha creado el registro de forma satisfactoria: ' + req.params.nombre);
+
+    return next();
+});
+
+servidor.listen(4949, function() {
+    console.log(`${servidor.name} se está ejecutando en ${servidor.url}.`);
+});
