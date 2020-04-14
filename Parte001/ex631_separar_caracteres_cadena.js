b30360aa2b8182cc82de3ffe105e334505704557
infzer0 <johnortizo@outlook.com>
2020-04-14 16:01:46 -0500

    JavaScript - Ejercicio 631
    JavaScript - Ejercicio 631: Usar el Operador de Expansión para Separar los Caracteres de una Cadena.

---
+++ Parte001/ex631_separar_caracteres_cadena.js
@@ -0,0 +1,12 @@
+// Ejercicio 631: Usar el operador de expansión para separar los caracteres de una cadena.
+
+// Spread operator
+
+// "JavaScript" => ['J', 'a', 'v', 'a', 'S', 'c', 'r', 'i', 'p', 't']
+
+let texto = 'JavaScript';
+
+let caracteres = [...texto];
+
+console.log('Contenido de la variable texto:', texto);
+console.log('Contenido de la variable caracteres:', caracteres);
