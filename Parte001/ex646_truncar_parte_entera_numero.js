7f6bc3293233372b742aa3b797bc5c65a19fc750
infzer0 <johnortizo@outlook.com>
2020-04-19 09:09:36 -0500

    JavaScript - Ejercicio 646
    JavaScript - Ejercicio 646: Usar la Función Math.trunc() para Truncar un Número de Punto Flotante.

---
+++ Parte001/ex646_truncar_parte_entera_numero.js
@@ -0,0 +1,19 @@
+// Ejercicio 646: Usar la función Math.trunc() para truncar un número de punto flotante.
+
+console.log(Math.trunc(13.37));
+// expected output: 13
+
+console.log(Math.trunc(42.84));
+// expected output: 42
+
+console.log(Math.trunc(0.123));
+// expected output: 0
+
+console.log(Math.trunc(-0.123));
+// expected output: -0
+
+console.log(Math.trunc(Math.PI));
+// expected output: 3
+
+console.log(Math.trunc(Math.E));
+// expected output: 2
