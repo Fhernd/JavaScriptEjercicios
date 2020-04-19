7379b64fddd9d8c140770799d3c1906e37b0c36e
infzer0 <johnortizo@outlook.com>
2020-04-19 09:22:48 -0500

    JavaScript - Ejercicio 647
    JavaScript - Ejercicio 647: Uso de la Función Math.sign() para Encontrar el Signo de un Número.

---
+++ Parte001/ex647_signo_numero.js
@@ -0,0 +1,13 @@
+// Ejercicio 647: Uso de la función Math.sign() para encontrar el signo de un número.
+
+console.log(Math.sign(3));
+// expected output: 1
+
+console.log(Math.sign(-3));
+// expected output: -1
+
+console.log(Math.sign(0));
+// expected output: 0
+
+console.log(Math.sign('-3'));
+// expected output: -1
