79dd53a927c06c1829953dc3bec99fee065e0884
infzer0 <johnortizo@outlook.com>
2020-09-15 08:10:52 -0500

    JavaScript - Ejercicio 850: Demostrar el Uso Básico de la Función substr() del Objeto String
    Demostrar el Uso Básico de la Función substr() del Objeto String con el lenguaje de programación JavaScript.

---
+++ Parte001/ex850-demo-uso-funcion-substr.js
@@ -0,0 +1,15 @@
+// Ejercicio 850: Demostrar el uso de la función substr() del objeto String.
+
+// Resumen: para crear una subcadena (subtexto) de una cadena de caracteres.
+
+let lenguaje = 'JavaScript es tremendo';
+console.log(lenguaje);
+
+console.log();
+
+let javascript = lenguaje.substr(0, 10);
+console.log(javascript);
+
+console.log();
+
+console.log(lenguaje.substr(10));
