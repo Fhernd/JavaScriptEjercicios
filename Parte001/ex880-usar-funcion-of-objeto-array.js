b1d4986475709bc41ad71c11d3d1a5dcd2178e7d
infzer0 <johnortizo@outlook.com>
2020-10-10 17:52:04 -0500

    JavaScript - Ejercicio 880: Demostrar el Uso de la Función Array.of() para Crear un Arreglo
    Demostrar el Uso de la Función Array.of() para Crear un Arreglo con el lenguaje de programación JavaScript.

---
+++ Parte001/ex880-usar-funcion-of-objeto-array.js
@@ -0,0 +1,13 @@
+// Ejercicio 880: Demostrar el uso de la función Array.of() para crear un arreglo.
+
+// Array.of(elemento0, [, elemento1, [elemento2, ...[, elementoN]]])
+
+let numeros = Array.of(5);
+console.log(numeros.length);    // 1
+console.log(numeros); // [ 5 ]
+
+console.log();
+
+let lenguajes = Array.of('Python', 'JavaScript', 'Java', 'C++', 'Go');
+console.log(lenguajes.length);  // 5
+console.log(lenguajes);
