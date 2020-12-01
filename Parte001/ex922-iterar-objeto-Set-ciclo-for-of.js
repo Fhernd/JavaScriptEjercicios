4efa9697297955d3e348cb70e42744065802fec7
infzer0 <johnortizo@outlook.com>
2020-12-01 16:18:44 -0500

    JavaScript - Ejercicio 922: Iterar Todos los Elementos de un Objeto Set con un Ciclo for...of
    Iterar Todos los Elementos de un Objeto Set con un Ciclo for...of con el lenguaje de programación JavaScript.

---
+++ Parte001/ex922-iterar-objeto-Set-ciclo-for-of.js
@@ -0,0 +1,7 @@
+// Ejercicio 922: Iterar a través de un objeto Set con un ciclo for...of.
+
+let planetas = new Set(['Mercurio', 'Venus', 'Tierra', 'Marte', 'Jupiter', 'Saturno', 'Urano', 'Neptuno']);
+
+for(const planeta of planetas) {
+    console.log(planeta);
+}
