a940ebb276022c2cb2ebf1f83b26d41f6e5ad3f8
infzer0 <johnortizo@outlook.com>
2020-12-02 06:24:05 -0500

    JavaScript - Ejercicio 923: Usar una Literal de Plantilla para Organizar Texto en Diferentes Líneas de Código
    Usar una Literal de Plantilla para Organizar Texto en Diferentes Líneas de Código con el lenguaje de programación JavaScript.

---
+++ Parte001/ex923-texto-multilinea-literales-plantilla.js
@@ -0,0 +1,16 @@
+// Ejercicio 923: Usar una literal de plantilla para organizar texto en diferentes líneas de código.
+
+let extracto = `
+Veces me quedo subía los sólo de heridas me cosas.
+
+Las que y tierra todo nadadora escaleras olvidando, tierra nadadora acaba yo repartiendo brooklyn acaba, signos la escobazos criaturas que.
+
+Que tierra con faraón es subía horizonte. Que de nino arroyo sus nadadora con. Con ni pensamiento tierra borrachos los atrás me lentejas, mi del los para abrir pasan un borrachos, a larga tierra todo vuelve con, es huido los me las y no y, desnuda húmedo osos muerte tierra que mujer tu inocentes vacía.
+
+Los muertos de las la los tierra, lentejas me de tierra brooklyn que lenguas los.
+
+
+Dirges at raven the unbrokenquit here ominous metell, dream but i flown this his here streaming and. Saintly ah devil back this sure, smiling lamplight oer my whether i by, into said name my beguiling of velvet devil mystery. Before above scarce lining in december, croaking and within raven fowl rare mystery aidenn, but the raven chamber perched in shadows shadows then lamplight, beguiling spoken at vainly so distant my kind bird my, wheeled lie that shadows he to. Whose take this a with. The soul ever door you one seeming bird. Sitting now let grim bosoms felt the came. No to only fiery name distant angels on as, i truly bust the see tempter entrance tapping the, grave the my chamber nepenthe nothing but kind. Other pallid here i whispered, for this pallas more of for quoth, whether let and stopped that, of cannot i loneliness rare still so this the dreary. Fantastic adore if divining oer bust footfalls hopes. Clasp the with grave chamber i nothing flown core, then ever madam burned fantastic this some. By turning maiden burden i one head. I this on forget and oh back and something. Till the i it shadow of shorn.
+`
+
+console.log(extracto);
