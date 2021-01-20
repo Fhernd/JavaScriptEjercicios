9c5bca6feb5c28036ddc3c2ef186cb971b8f70b0
John Ortiz Ordoñez <johnortizo@outlook.com>
2021-01-20 14:57:04 -0500

    JavaScript - Ejercicio 946: Crear una Función para Eliminar n Elementos desde el Inicio de un Arreglo
    Crear una Función para Eliminar n Elementos desde el Inicio de un Arreglo con el lenguaje de programación JavaScript.

---
+++ Parte001/ex946-tomar-n-cantidad-elementos-arreglo-funcion.js
@@ -0,0 +1,55 @@
+// Ejercicio 946: Crear una función para eliminar n elementos desde el inicio de un arreglo.
+
+function eliminar(datos, n = 1) {
+    if (!Array.isArray(datos)) {
+        throw TypeError('El argumento «datos» debe ser un arreglo.');
+    }
+
+    if (typeof n != 'number') {
+        throw TypeError('El argumento «n» debe ser un número.');
+    }
+
+    if (!Number.isInteger(n)) {
+        throw TypeError('El argumento «n» debe ser un número entero.');
+    }
+
+    return datos.slice(0, n);
+}
+
+try {
+    console.log(eliminar([1, 2, 3, 4, 5], 2));  // [1, 2]
+} catch (e) {
+    console.log(`Error: ${e.message}`);
+}
+
+console.log();
+
+try {
+    console.log(eliminar([1, 2, 3, 4, 5], 4));  // [1, 2, 3, 4]
+} catch (e) {
+    console.log(`Error: ${e.message}`);
+}
+
+console.log();
+
+try {
+    console.log(eliminar([1, 2, 3, 4, 5], 10));  // [1, 2, 3, 4, 5]
+} catch (e) {
+    console.log(`Error: ${e.message}`);
+}
+
+console.log();
+
+try {
+    console.log(eliminar(1000, 10));  // Error
+} catch (e) {
+    console.log(`Error: ${e.message}`);
+}
+
+console.log();
+
+try {
+    console.log(eliminar([1, 2, 3, 4, 5], 5.5));  // Error
+} catch (e) {
+    console.log(`Error: ${e.message}`);
+}
