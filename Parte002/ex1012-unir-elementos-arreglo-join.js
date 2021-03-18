c17d31c22e7e8c00dc2fc88906a83c92f8955f4f
infzer0 <johnortizo@outlook.com>
2021-03-18 10:24:49 -0500

    JavaScript Ejercicio: 1012 Usar la Función join() para Unir con un Carácter los Elementos de un Arreglo
    Usar la Función join() para Unir con un Carácter los Elementos de un Arreglo con el lenguaje de programación JavaScript.

---
+++ Parte002/ex1012-unir-elementos-arreglo-join.js
@@ -0,0 +1,17 @@
+// Ejercicio 1012: Usar la función join() para unir los elementos de un arreglo.
+
+let paises = ['Colombia', 'Perú', 'Venezuela', 'Chile', 'Argentina'];
+
+console.log(paises);
+
+console.log();
+
+console.log(paises.toString());
+
+console.log();
+
+console.log(paises.join());
+console.log(paises.join(','));
+console.log(paises.join('-'));
+console.log(paises.join(';'));
+console.log(paises.join('_'));
