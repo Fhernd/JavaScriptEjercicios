3f5b2abfa2a3041739ee081e8bb315ffd8f79e8f
infzer0 <johnortizo@outlook.com>
2021-04-14 07:56:57 -0500

    JavaScript Ejercicio: 1035 Usar la Función para Unir/Concatenar Cadenas de Caracteres
    Usar la Función para Unir/Concatenar Cadenas de Caracteres con el lenguaje de programación JavaScript.

---
+++ Parte002/ex1035-concatenar-texto-con-concat().js
@@ -0,0 +1,17 @@
+// Ejercicio 1035: Usar la función concat() para unir/concatenar cadenas de caracteres.
+
+let frase = 'JavaScript';
+console.log(frase);
+console.log(frase.length);
+
+console.log();
+
+frase = frase.concat(' es');
+console.log(frase);
+console.log(frase.length);  // 13
+
+console.log();
+
+frase = frase.concat(' un', ' lenguaje ', 'de ', 'programación');
+console.log(frase);
+console.log(frase.length);  // ...
