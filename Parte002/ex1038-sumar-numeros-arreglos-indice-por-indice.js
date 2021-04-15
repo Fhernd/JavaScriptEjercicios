7decf8ea55a0cadd0295064cb817cb5a18817001
infzer0 <johnortizo@outlook.com>
2021-04-15 17:47:24 -0500

    JavaScript Ejercicio: 1038 Sumar los Números de Dos Arreglos Índice a Índice
    Sumar los Números de Dos Arreglos Índice a Índice con el lenguaje de programación JavaScript.

--- Parte002/ex1038-sumar-numeros-arreglos-indice-por-indice.js
+++ Parte002/ex1038-sumar-numeros-arreglos-indice-por-indice.js
@@ -1,3 +1,14 @@
 // Ejercicio 1038: Sumar los números de dos arreglos índice a índice.
 
+let numeros1 = [1, 2, 3, 4, 5];
+let numeros2 = [6, 7, 8, 9, 10, 11, 12, 13];
 
+let menorLongitud = Math.min(numeros1.length, numeros2.length);
+
+let resultado = [];
+
+for(let i = 0; i < menorLongitud; ++i) {
+    resultado.push(numeros1[i] + numeros2[i]);
+}
+
+console.log(resultado); // [7, 9, 11, 13, 15]
