4b358804a9cbf8f8e35ac486e8584221d0fba8f5
infzer0 <johnortizo@outlook.com>
2021-04-27 07:14:25 -0500

    JavaScript Ejercicio: 1051 Obtener la Fecha y Hora Actuales con el Objeto Incorporado Date
    Obtener la Fecha y Hora Actuales con el Objeto Incorporado Date con el lenguaje de programación JavaScript.

---
+++ Parte002/ex1051-obtener-fecha-actual.js
@@ -0,0 +1,10 @@
+// Ejercicio 1051: Obtener la fecha actual a través de un objeto de la clase incorporada Date().
+
+let fechaHoraActual = new Date();
+console.log(fechaHoraActual);
+
+console.log();
+
+console.log(fechaHoraActual.getDate());
+console.log(fechaHoraActual.getMonth() + 1);
+console.log(fechaHoraActual.getFullYear()); // 2021
