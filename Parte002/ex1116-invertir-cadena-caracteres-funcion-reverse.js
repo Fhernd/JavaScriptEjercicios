24a7bc3be9dc9fbe9fb780f46f2dd2ea734e909b
infzer0 <johnortizo@outlook.com>
2021-06-01 15:50:51 -0500

    JavaScript Ejercicio: 1116 Invertir una Cadena de Caracteres (String) con la Función reverse() de Array
    Invertir una Cadena de Caracteres (String) con la Función reverse() de Array con el lenguaje de programación JavaScript.

---
+++ Parte002/ex1116-invertir-cadena-caracteres-funcion-reverse.js
@@ -0,0 +1,17 @@
+// Ejercicio 1116: Invertir una cadena de caracteres utilizando la función reverse().
+
+// JavaScript => tpircSavaJ
+
+let lenguaje = 'JavaScript';
+
+console.log(lenguaje);
+
+console.log();
+
+// ['J', 'a', 'v', 'a', 'S', ...]
+// ['t', 'p', 'i', ...]
+// tpircSavaJ
+
+let lenguajeInvertido = lenguaje.split('').reverse().join('');
+
+console.log(lenguajeInvertido);
