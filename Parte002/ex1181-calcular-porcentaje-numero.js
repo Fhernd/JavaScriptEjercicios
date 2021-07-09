236aed2063646aae5099973ba392e00ce788dd65
infzer0 <johnortizo@outlook.com>
2021-07-09 15:26:48 -0500

    JavaScript Ejercicio: 1181 Escribir una Función Personalizada para Calcular el Porcentaje de un Número
    Escribir una Función Personalizada para Calcular el Porcentaje de un Número con el lenguaje de programación JavaScript.

---
+++ Parte002/ex1181-calcular-porcentaje-numero.js
@@ -0,0 +1,8 @@
+// Ejercicio 1181: Escribir una función para calcular el porcentaje de un número.
+
+function calcularPorcentaje(numero, porcentaje) {
+    return numero / 100 * porcentaje;
+}
+
+console.log(calcularPorcentaje(1000, 20));  // 200
+console.log(calcularPorcentaje(2000, 15));  // 300
