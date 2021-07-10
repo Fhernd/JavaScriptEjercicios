b4a51a0876b1c1a27afae4d233a98b9448ca02ab
infzer0 <johnortizo@outlook.com>
2021-07-10 10:25:33 -0500

    JavaScript Ejercicio: 1182 Definir una Función a la Medida para Convertir Grados en Radianes
    Definir una Función a la Medida para Convertir Grados en Radianes con el lenguaje de programación JavaScript.

---
+++ Parte002/ex1182-convertir-grados-radianes-funcion.js
@@ -0,0 +1,11 @@
+// Ejercicio 1182: Definir una función a la medida para convertir grados en radianes.
+
+function convertirGradosRadianes(grados) {
+    return grados * (Math.PI / 180);
+}
+
+console.log(convertirGradosRadianes(90));   // Math.PI / 2
+
+console.log(convertirGradosRadianes(180));   // Math.PI
+
+console.log(convertirGradosRadianes(270));
