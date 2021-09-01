528abd2a886e96bdc79e95a5e89db72c611e8381
infzer0 <johnortizo@outlook.com>
2021-09-01 18:34:26 -0500

    JavaScript Ejercicio: 1210 Usar el Operador de Propagación para Clonar el Contenido de un Arreglo
    Usar el Operador de Propagación para Clonar el Contenido de un Arreglo con el lenguaje de programación JavaScript.

---
+++ Parte002/ex1210-clonar-arreglo-con-spread-operator.js
@@ -0,0 +1,11 @@
+// Ejercicio 1210: Usar el operador de propagación para clonar el contenido entero de un arreglo.
+
+let primos = [2, 3, 5, 7];
+console.log(primos);
+console.log(primos.length);
+
+console.log();
+
+let copiaPrimos = [...primos];
+console.log(copiaPrimos);
+console.log(copiaPrimos.length);
