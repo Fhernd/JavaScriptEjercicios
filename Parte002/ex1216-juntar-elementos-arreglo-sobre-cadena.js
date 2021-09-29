870bded2ba183dfd92963eecfec7c6c3416e8de2
infzer0 <johnortizo@outlook.com>
2021-09-29 17:23:42 -0500

    JavaScript Ejercicio: 1216 Juntar o Unir los Elementos de un Arreglo con la Función join()
    Juntar o Unir los Elementos de un Arreglo con la Función join() con el lenguaje de programación JavaScript.

---
+++ Parte002/ex1216-juntar-elementos-arreglo-sobre-cadena.js
@@ -0,0 +1,13 @@
+// Ejercicio 1216: Juntar los elementos de un arreglo con la función join().
+
+let paises = ['Colombia', 'Corea del Sur', 'México', 'Perú', 'Rusia'];
+console.log(paises);
+
+console.log();
+
+console.log(paises.toString());
+console.log(paises.join());
+console.log(paises.join(','));
+console.log(paises.join(';'));
+console.log(paises.join('-'));
+console.log(paises.join('|'));
