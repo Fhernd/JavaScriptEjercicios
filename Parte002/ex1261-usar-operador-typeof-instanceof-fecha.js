47301d2f14a01580a50e0c795b6ae46b1d25d480
infzer0 <johnortizo@outlook.com>
2021-10-21 10:34:22 -0500

    JavaScript Ejercicios: 1261 Usar los Operadores typeof e instanceof sobre Objetos de Tipo Fecha (Date)
    Usar los Operadores typeof e instanceof sobre Objetos de Tipo Fecha (Date) con el lenguaje de programación JavaScript.

---
+++ Parte002/ex1261-usar-operador-typeof-instanceof-fecha.js
@@ -0,0 +1,13 @@
+// Ejercicio 1261: Usar los operadores typeof e instanceof para comprobar el tipo de dato de un objeto Date.
+
+let fechaHoraActual = new Date();
+
+console.log(fechaHoraActual);
+
+console.log();
+
+console.log(typeof fechaHoraActual);    // object
+
+console.log();
+
+console.log(fechaHoraActual instanceof Date);   // true
