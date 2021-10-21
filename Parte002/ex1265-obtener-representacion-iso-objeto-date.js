57ba7c059dce99c67331416d9dca52256420221e
infzer0 <johnortizo@outlook.com>
2021-10-21 11:29:12 -0500

    JavaScript Ejercicio: 1265 Usar la Función toISOString para Obtener la Fecha en Formato ISO
    Usar la Función toISOString para Obtener la Fecha en Formato ISO con el lenguaje de programación JavaScript.

---
+++ Parte002/ex1265-obtener-representacion-iso-objeto-date.js
@@ -0,0 +1,9 @@
+// Ejercicio 1265: Usar la función toISOString() para obtener la representación en texto de una fecha.
+
+let fechaHoraActual = new Date();
+
+console.log(fechaHoraActual);
+
+console.log();
+
+console.log(fechaHoraActual.toISOString());
