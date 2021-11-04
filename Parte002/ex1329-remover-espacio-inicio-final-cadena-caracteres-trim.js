1ccbc8dd0737f5102dd5efa3f041265da3b7cab7
infzer0 <johnortizo@outlook.com>
2021-11-04 18:57:41 -0500

    JavaScript Ejercicio: 1329 Remover el Espacio en Blanco al Inicio y Final de un Texto con la Función trim()
    Remover el Espacio en Blanco al Inicio y Final de un Texto con la Función trim() con el lenguaje de programación JavaScript.

---
+++ Parte002/ex1329-remover-espacio-inicio-final-cadena-caracteres-trim.js
@@ -0,0 +1,11 @@
+// Ejercicio 1329: Remover el espacio en blanco al inicio y al final de un texto con trim().
+
+let texto = '              OrtizOL                                ';
+console.log(texto);
+console.log(texto.length);
+
+console.log();
+
+let resultado = texto.trim();
+console.log(resultado);
+console.log(resultado.length);
