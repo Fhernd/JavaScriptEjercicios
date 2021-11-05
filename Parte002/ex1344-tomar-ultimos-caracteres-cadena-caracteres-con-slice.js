86037818dbbed8fbfd4caae6d31b0e1c81decdc2
infzer0 <johnortizo@outlook.com>
2021-11-05 16:48:22 -0500

    JavaScript Ejercicio: 1344 Tomar los N Últimos Caracteres de un Texto con la Función slice()
    Tomar los N Últimos Caracteres de un Texto con la Función slice() con el lenguaje de programación JavaScript.

---
+++ Parte002/ex1344-tomar-ultimos-caracteres-cadena-caracteres-con-slice.js
@@ -0,0 +1,9 @@
+// Ejercicio 1344: Tomar los N últimos caracteres de un texto con la función slice().
+
+let lenguaje = 'JavaScript es tremendo.';
+
+console.log(lenguaje);
+
+console.log(lenguaje.slice(-5));    // endo.
+console.log(lenguaje.slice(-6));    // mendo.
+console.log(lenguaje.slice(-10));    //  tremendo.
