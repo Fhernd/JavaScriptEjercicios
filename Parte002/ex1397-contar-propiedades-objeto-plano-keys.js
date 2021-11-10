8b61912039676f1d58c4013cd7031ea9bf0672d5
infzer0 <johnortizo@outlook.com>
2021-11-10 18:45:02 -0500

    JavaScript Ejercicio: 1397 Contar con la Función keys() las Propiedades de un Objeto Plano
    Contar con la Función keys() las Propiedades de un Objeto Plano con lenguaje de programación JavaScript.

---
+++ Parte002/ex1397-contar-propiedades-objeto-plano-keys.js
@@ -0,0 +1,10 @@
+// Ejericicio 1397: Contar con la función keys() las propiedades de un objeto plano.
+
+let computador = {
+    id: 1001,
+    marca: 'MSi',
+    cpu: 'Intel Core i7',
+    ram: '16GB'
+};
+
+console.log(Object.keys(computador).length);    // 4
