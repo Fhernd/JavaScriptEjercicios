e5b5aa2d1d4fe0e769a18d349a7e24af7c4aa51a
infzer0 <johnortizo@outlook.com>
2021-11-13 19:27:16 -0500

    JavaScript Ejercicio: 1425 Demostrar el Uso de la Función at() de los Objetos String
    Demostrar el Uso de la Función at() de los Objetos String con el lenguaje de programación JavaScript.

---
+++ Parte002/ex1425-demo-uso-funcion-at-string.js
@@ -0,0 +1,8 @@
+// Ejercicio 1425: Demostrar el uso de la función at() de los objetos String.
+
+// String.prototype.at() -> obtiene el carácter en un índice dado.
+
+let lenguaje = 'JavaScript';
+
+console.log(lenguaje.at(0));    // 'J'
+console.log(lenguaje.at(-1));   // 't'
