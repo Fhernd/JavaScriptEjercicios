c3ac1e487a6bb15b24d69b0f9138395687cb42e8
John Ortiz Ordoñez <jf.ortiz45@uniandes.edu.co>
2022-09-14 20:11:20 -0500

    JavaScript Ejercicio: 1464 Instalación y Demostración de la Extensión GitHub Copilot en Visual Studio Code
    Instalación y Demostración de la Extensión GitHub Copilot en Visual Studio Code.

---
+++ Parte002/ex1464-demo-github-copilot.js
@@ -0,0 +1,29 @@
+// Ejercicio 1464: Demostración Básica de GitHub Copilot.
+
+// Función para sumar dos números enteros:
+
+function sumar(a, b) {
+    return a + b;
+}
+
+// Function to tweet a custom message using an API:
+function tweet(message) {
+    // ...
+}
+
+// Create a class to represent an employee:
+class Employee {
+    constructor(name, title) {
+        this.name = name;
+        this.title = title;
+    }
+}
+
+// Detect the load event:
+window.addEventListener('load', function() {
+    // Display a message in the console:
+    console.log('The page has loaded.');
+
+    // Mostrar otro mensaje en español en la consola:
+    console.log('La página ha cargado.');
+});
